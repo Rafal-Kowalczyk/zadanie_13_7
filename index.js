05f0e21c76190a34a2bbb38cbc1035b428baf494
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-13 23:29:02 +0100

    Add function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-﻿
+﻿var fs = require('fs');
+
+fs.readdir('./test/', 'utf-8', function(err, files) {
+    console.log(files);
+    fs.writeFile('copy.txt', files, function(err) {
+        if (err) throw err;
+        console.log('Read content saved to the file copy.txt');
+    });
+});
